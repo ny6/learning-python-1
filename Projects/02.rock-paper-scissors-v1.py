a1babe886d0fd578cc2772c6a4c3f278b227055b
Yeshu Kochher <yeshu@outlook.in>
2019-04-16 19:10:22 +0530

    one where rock, paper, scissors v2

--- Projects/02.rock-paper-scissors-v1.py
+++ Projects/02.rock-paper-scissors-v1.py
@@ -13,18 +13,38 @@ m1 = 'Player 1 wins!'
 m2 = 'Player 2 wins!'
 
 if p1 == p2:
-  print('Match draw!')
-elif p1 == ROCK and p2 == PAPER:
-  print(m2)
-elif p1 == ROCK and p2 == SCISSORS:
-  print(m1)
-elif p1 == PAPER and p2 == ROCK:
-  print(m1)
-elif p1 == PAPER and p2 == SCISSORS:
-  print(m2)
-elif p1 == SCISSORS and p2 == ROCK:
-  print(m2)
-elif p1 == SCISSORS and p2 == PAPER:
-  print(m1)
+  print('It is a tie!')
+elif p1 == ROCK:
+  if p2 == PAPER:
+    print(m2)
+  elif p2 == SCISSORS:
+    print(m1)
+elif p1 == PAPER:
+  if p2 == ROCK:
+    print(m1)
+  elif p2 == SCISSORS:
+    print(m2)
+elif p1 == SCISSORS:
+  if p2 == ROCK:
+    print(m2)
+  elif p2 == PAPER:
+    print(m1)
 else:
-  print('Incorrect value found!')
+  print('Invalid option!')
+
+# if p1 == p2:
+#   print('Match draw!')
+# elif p1 == ROCK and p2 == PAPER:
+#   print(m2)
+# elif p1 == ROCK and p2 == SCISSORS:
+#   print(m1)
+# elif p1 == PAPER and p2 == ROCK:
+#   print(m1)
+# elif p1 == PAPER and p2 == SCISSORS:
+#   print(m2)
+# elif p1 == SCISSORS and p2 == ROCK:
+#   print(m2)
+# elif p1 == SCISSORS and p2 == PAPER:
+#   print(m1)
+# else:
+#   print('Incorrect value found!')
